fe10469a7c329082b54312fefb1ab6aaed0f6918
thetahayaseen <thetahayaseen>
2023-10-18 07:25:22 -0700

    Fixed View Handling Based On User Status

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:fanatic/firebase_options.dart';
 import 'package:fanatic/views/login_view.dart';
 import 'package:fanatic/views/register_view.dart';
+import 'package:fanatic/views/verify_email_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -29,13 +31,16 @@ class HomePage extends StatelessWidget {
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text("Email Is Verified...");
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user?.emailVerified ?? false) {
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
           default:
             return const CircularProgressIndicator();
         }
