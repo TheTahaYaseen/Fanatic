0df1712e849f5b0a47989771a428452c73618335
thetahayaseen <thetahayaseen>
2023-10-18 07:19:07 -0700

    Added Loading Animation!

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class HomePage extends StatelessWidget {
             // }
             return const LoginView();
           default:
-            return const Text("Loading...");
+            return const CircularProgressIndicator();
         }
       },
       future: Firebase.initializeApp(
